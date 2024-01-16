dcd515a7159d8457a271b2b2134940084393655f
GloodBuster <95661756+GloodBuster@users.noreply.github.com>
2024-01-15 22:53:15 -0400

    add spacy summarization

--- nlp_backend/summarization.py
+++ nlp_backend/summarization.py
@@ -5,6 +5,7 @@ from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lsa import LsaSummarizer as Summarizer
 from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
+import spacy
 
 
 def summarize_text_with_heapq(text, language='spanish', summary_length=30):
@@ -55,6 +56,46 @@ def summarize_text_with_sumy(text, language='spanish', summary_length=30):
     for sentence in summarizer(parser.document, summary_length):
         summary_sentences.append(sentence._text)
 
+    summary = ' '.join(summary_sentences)
+
+    return summary
+
+def summarize_text_with_spacy(text, language='es_core_news_md', summary_length=30):
+    # Load the SpaCy model
+    nlp = spacy.load(language)
+
+    # Process the text
+    doc = nlp(text)
+
+    # Get the word frequencies
+    word_frequencies = {}
+    for token in doc:
+        if not token.is_stop and not token.is_punct:
+            if token.text not in word_frequencies.keys():
+                word_frequencies[token.text] = 1
+            else:
+                word_frequencies[token.text] += 1
+
+    # Normalize the frequencies
+    maximum_frequency = max(word_frequencies.values())
+    for word in word_frequencies.keys():
+        word_frequencies[word] = (word_frequencies[word] / maximum_frequency)
+
+    # Score the sentences
+    sentence_scores = {}
+    for sent in doc.sents:
+        for token in sent:
+            if token.text in word_frequencies.keys():
+                if len(sent.text.split(' ')) < summary_length:
+                    if sent.text not in sentence_scores.keys():
+                        sentence_scores[sent.text] = word_frequencies[token.text]
+                    else:
+                        sentence_scores[sent.text] += word_frequencies[token.text]
+
+    # Get the highest scoring sentences
+    summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
+
+    # Join the sentences together
     summary = ' '.join(summary_sentences)
 
     return summary
