d36131efd7f100aded6c8b482c24753736f32e94
GloodBuster <95661756+GloodBuster@users.noreply.github.com>
2024-01-17 11:06:10 -0400

    fix fastApi backend

--- nlp_backend/testApi.py
+++ nlp_backend/testApi.py
@@ -10,37 +10,17 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 app = FastAPI()
 
+
 def get_db_connection():
-    return psycopg2.connect(DATABASE_URL,  options='-c client_encoding=latin-1', encoding='latin-1')
+    conn = psycopg2.connect(DATABASE_URL, options='-c client_encoding=latin-1')
+    conn.set_client_encoding('latin-1')
+    return conn
 
-@app.get("/works", response_model=List[dict])
-async def get_works():
+
+@app.get("/works/{id}")
+async def get_works(id: int, limit: int = 10, offset: int = 0):
     try:
-        conn = get_db_connection()
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM works")
-        results = cur.fetchall()
-        cur.close()
-        conn.close()
-
-        works_list = []
-        for row in results:
-            print("Raw id value:", row[0])
-            work_dict = {
-                "id": str(row[0]),
-                "genre": row[1],
-                "title": row[2],
-                "description": row[3].decode('utf-8', 'ignore'),
-                "meter": row[4],
-                "subject": row[5],
-                "original_language": row[6],
-                "elaboration_start_date": str(row[7]),
-                "elaboration_end_date": str(row[8]),
-                "elaboration_places": row[9],
-            }
-            works_list.append(work_dict)
-
-        return works_list
+        return {"author": 'Peter Johnson', "id": id, "limit": limit, "offset": offset}
 
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
